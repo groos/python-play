45b9dbc010864cbe7c59f87d4bc6841db7fd1548
Nick Groos <ntgroos@gmail.com>
2015-01-26 21:42:09 -0600

    prints out some reddit content

---
+++ readme.txt
@@ -0,0 +1,1 @@
+Messing with some Python libraries.
--- rss-play.py
+++ rss-play.py
@@ -1,5 +1,17 @@
 import feedparser
 
+f = feedparser.parse('http://www.reddit.com/r/minecraft/.rss')
+
+print f.feed.title
+
+print '''
+Notch has invited you over for a party at his new Beverly Hills Mansion.
+See below for details :D
 '''
 
-'''
+print f.feed.subtitle
+
+for entrie in f.entries:
+    print '>>' + entrie.title + '\n  ' + entrie.description
+    print '>>' + entrie.link + '\n~~~~~~~~~~~~~~~~~~~~~~~~~~\n'
+
